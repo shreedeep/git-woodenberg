57cedd3a26f8a83812e347eb314d5a9486356e5e
shreedeep <shreedeepak8@gmail.com>
2018-09-06 18:30:19 +0530

    added consort class and change in App.java

--- src/main/java/com/ibm/git_demo/App.java
+++ src/main/java/com/ibm/git_demo/App.java
@@ -8,7 +8,7 @@ public class App
 {
 	public void addData(String data) {
 		System.out.println("data="+data);
-		if(data.equals("java gems")) {
+		if(data.equals(Consort.DATA_TYPE)) {
 			System.out.println("Welcome");
 		}
 	}
---
+++ src/main/java/com/ibm/git_demo/Consort.java
@@ -0,0 +1,6 @@
+package com.ibm.git_demo;
+
+public class Consort {
+	public static String DATA_TYPE="Java gems";
+
+}

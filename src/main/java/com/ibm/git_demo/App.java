9c700a5511f1ae109b92d4cff253bb0c974b99b6
shreedeep <shreedeepak8@gmail.com>
2018-09-06 17:53:07 +0530

    added if condition to check the data

--- src/main/java/com/ibm/git_demo/App.java
+++ src/main/java/com/ibm/git_demo/App.java
@@ -8,6 +8,9 @@ public class App
 {
 	public void addData(String data) {
 		System.out.println("data="+data);
+		if(data.equals("java gems")) {
+			System.out.println("Welcome");
+		}
 	}
     public static void main( String[] args )
     {
